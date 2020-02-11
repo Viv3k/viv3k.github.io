aa90028a1abbb349d8b118f7319a8a7503892e44
Vivek <vivek.darsanapu@inmobi.com>
2020-02-11 17:12:12 +0530

    Still cooking.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Still cooking 🍳 ...
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome
         </a>
       </header>
     </div>
