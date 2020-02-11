5e3f9ba2823b407b7b2dd5a98748bf6898a59c3d
Vivek <vivek.cs12b007@gmail.com>
2020-02-11 17:14:28 +0530

    Cooking in progress... (#3)

---
+++ precache-manifest.da25791b311b49117a1814c2bd89e880.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e78ff362db6adb022167394fb87fe6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b09b200a2897bfa948c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5f58f525a8c46557eb87",
+    "url": "/static/js/2.e3d9c8ad.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.e3d9c8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b09b200a2897bfa948c",
+    "url": "/static/js/main.9403683f.chunk.js"
+  },
+  {
+    "revision": "901ec6ab651bcc48f88a",
+    "url": "/static/js/runtime-main.cd817de0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
